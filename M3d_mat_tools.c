05a2c5cc11be936655c527395a5e687529a53a51
Nic Richardson <nrichardson@lclark.edu>
2019-10-29 10:13:45 -0700

    m3d

---
+++ M3d_mat_tools.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdarg.h>
+
+
+/*
+
+ ( x')          (x)
+ ( y')  =   M * (y)  
+ ( 1 )          (1)
+
+instead of (x',y',1) = (x,y,1) * M  
+
+*/
+
+void M2d_print_mat(double a[4][4]) {
+  int r, c;
+  for (r = 0; r < 4; r++) {
+    printf("|");
+    for (c = 0; c < 4; c++) {
+      printf(" %12.4lf ", a[r][c]);
+    }
+    printf("|");
+    printf("\n");
+  }
+} 
+
+void M2d_copy_mat(double dest[4][4], double src[4][4]) {
+  for (int i = 0; i < 4; i++) { 
+    for (int j = 0; j < 4; j++) {
+      dest[i][j] = src[i][j];
+    }
+  }
+} 
+
+void M2d_make_identity(double a[4][4]) {
+  for (int r = 0; r < 4; r++) {
+    for (int c = 0; c < 4; c++) {
+      if (r == c) a[r][c] = 1.0;
+      else        a[r][c] = 0.0;
+    }
+  }
+} 
+
+void M2d_make_translation(double a[4][4], double dx, double dy, double dz) {
+  M2d_make_identity(a);
+  a[0][3] = dx;
+  a[1][3] = dy;
+  a[2][3] = dz;
+}
+
+void M2d_make_scaling(double a[4][4], double sx, double sy, double sz) {
+  for (int r = 0; r < 3; r++) {
+      for (int c = 0; c < 3; c++) {
+           if (r == c) a[r][c] = 1.0;
+           else        a[r][c] = 0.0;
+      }
+  }
+
+  a[0][0] = sx;
+  a[1][1] = sy;
+  a[2][2] = sz;
+}
+
+void M2d_make_x_rotation_cs(double a[4][4], double cs, double sn) {
+  // this one assumes cosine and sine are already known
+  a[0][0] = cs;   a[0][1] = -sn;   a[0][2] =  0;   a[0][3] =  0;
+  a[1][0] = sn;   a[1][1] =  cs;   a[1][2] =  0;   a[1][3] =  0;
+  a[2][0] =  0;   a[2][1] =   0;   a[2][2] =  1;   a[2][3] =  0;
+  a[3][0] =  0;   a[3][1] =   0;   a[3][2] =  0;   a[3][3] =  1;
+}
+
+void M2d_make_y_rotation_cs(double a[4][4], double cs, double sn) {
+  // this one assumes cosine and sine are already known
+  a[0][0] =  1;   a[0][1] =   0;   a[0][2] =   0;   a[0][3] =  0;
+  a[1][0] =  0;   a[1][1] =  cs;   a[1][2] = -sn;   a[1][3] =  0;
+  a[2][0] =  0;   a[2][1] =  sn;   a[2][2] =  cs;   a[2][3] =  0;
+  a[3][0] =  0;   a[3][1] =   0;   a[3][2] =   0;   a[3][3] =  1;
+}
+
+void M2d_make_z_rotation_cs(double a[4][4], double cs, double sn) {
+  // this one assumes cosine and sine are already known
+  a[0][0] =  1;   a[0][1] =   0;   a[0][2] =   0;   a[0][3] =   0;
+  a[1][0] =  0;   a[1][1] =   1;   a[1][2] =   0;   a[1][3] =   0;
+  a[2][0] =  0;   a[2][1] =   0;   a[2][2] =  cs;   a[2][3] = -sn;
+  a[3][0] =  0;   a[3][1] =   0;   a[3][2] =  sn;   a[3][3] =  cs;
+}
+
+
+void M2d_mat_mult(double res[4][4], double a[4][4], double b[4][4]) {
+  // res = a * b
+  // this is SAFE, i.e. the user can make a call such as 
+  // M2d_mat_mult(p,  p,q) or M2d_mat_mult(p,  q,p) or  M2d_mat_mult(p, p,p)
+  double u[4][4];
+  double v[4][4];
+  M2d_copy_mat(u, a);
+  M2d_copy_mat(v, b);
+
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      res[i][j] = u[i][0] * v[0][j]
+                + u[i][1] * v[1][j]
+                + u[i][2] * v[2][j]
+		+ u[i][3] * v[3][j];
+    }
+  }
+
+void M2d_mat_mult_pt(double P[3], double m[4][4], double Q[3]) {
+  // P = m*Q
+  // SAFE, user may make a call like M2d_mat_mult_pt (W, m,W);
+  double u[2];
+  u[0] = Q[0];
+  u[1] = Q[1];
+  for (int i = 0; i < 2; i++) {
+    P[i] = m[i][0] * u[0]
+         + m[i][1] * u[1]
+         + m[i][2] * 1   ;
+  }
+}
+
+void M2d_mat_mult_points(double X[], double Y[], double Z[],
+                         double m[4][4],
+                         double x[], double y[], double z[], int numpoints) {
+  // |X0 X1 X2 ...|       |x0 x1 x2 ...|
+  // |Y0 Y1 Y2 ...| = m * |y0 y1 y2 ...|
+  // |Z0 Z1 Z2 ...|       |z0 z1 z2 ...|
+  // | 1  1  1 ...|       | 1  1  1 ...|
+
+  // SAFE, user may make a call like M2d_mat_mult_points (x,y, m, x,y, n);
+  double copyX[numpoints];
+  double copyY[numpoints];
+  double copyZ[numpoints];
+
+  for (int i = 0; i < numpoints; i++) {
+    copyX[i] = x[i];
+    copyY[i] = y[i];
+    copyZ[i] = z[i];
+  }
+
+  for (int j = 0; j < numpoints; j++) {
+    X[j] = m[0][0] * copyX[j] + m[0][1] * copyY[j] + m[0][2];
+    Y[j] = m[1][0] * copyX[j] + m[1][1] * copyY[j] + m[1][2];
+    Z[j] = m[2][0] * copyX[j] + m[2][2] * copyZ[j] + m[2][2];
+
+  }
+}
---
+++ switchNic.c
@@ -0,0 +1,222 @@
+#include <stdio.h>i
+#include <FPT.h>
+#include <float.h>
+
+#include "M2d_matrix_tools.c"
+
+
+double Rot[3][3];
+int polygonCount[10];
+double storageMat[3][3];
+
+// Array of the actual points in each polygon
+double polygonXs[10][10000][10000];
+double polygonYs[10][10000][10000];
+double flex[10000][10000];
+int pointCount[10];
+
+// Array of the number of points in each polygon
+int polygonPointCounts[1000];
+
+//colors
+double colors[10000][3];
+
+// Color & display the polygon
+FILE *file;
+void colorPoly(int index){  
+G_rgb(1,1,1);
+G_clear();
+  for (int i = 0; i < polygonCount[index]; i++) {
+    G_rgb(colors[index][i][0], colors[i][1], colors[i][2]);
+    G_fill_polygon(polygonXs[index][i], polygonYs[i], polygonPointCounts[i]);
+  }
+}
+
+
+void arrayMin(double *ns, int n,
+              double *min) {
+  *min = DBL_MAX;
+  for (int i = 0; i < n; i++) {
+    if (ns[i] < *min) *min = ns[i];
+  }
+}
+
+void arrayMax(double *ns, int n,
+              double *max) {
+  *max = DBL_MIN;
+  for (int i = 0; i < n; i++) {
+    if (ns[i] > *max) *max = ns[i];
+  }
+}
+
+void collectPoints(char *filename, int index) {
+  G_rgb(1, 1, 1);
+  G_clear();
+
+   file = fopen(filename, "r");
+
+  if (file == NULL) {
+    printf("Can not open file: %s\n", filename);
+    exit(0);
+  }
+
+  fscanf(file, "%d", &pointCount[i]);
+
+  // Initialize (x, y) arrays to store points
+  double xs[pointCount[index]];
+  double ys[pointCount[index]];
+
+  for (int i = 0; i < pointCount; i++) {
+     fscanf(file, "%lf %lf", &xs[i], &ys[i]);
+  }
+
+  // Find the bounding box
+  double minX, maxX, minY, maxY;
+  arrayMin(xs, pointCount, &minX);
+  arrayMax(xs, pointCount, &maxX);
+  arrayMin(ys, pointCount, &minY);
+  arrayMax(ys, pointCount, &maxY);
+
+  double midX = (maxX + minX) / 2;
+  double midY = (maxY + minY) / 2;
+
+  double width = maxX - minX;
+  double height = maxY - minY;
+
+  // How much of the screen should the polygon take up
+  // (slightly more complicated than that)
+  double relPolygonSize = 0.6;
+
+  double scaleFactor;
+  if (width > height) {
+    scaleFactor = relPolygonSize * sWidth / width;
+  } else {
+    scaleFactor = relPolygonSize * sHeight / height;
+  }
+
+  // Scale the polygon
+  /*
+  for (int i = 0; i < pointCount; i++) {
+    xs[i] *= scaleFactor;
+    ys[i] *= scaleFactor;
+  }
+  */
+  //Scaling with Matrices
+  double scaleMatrix[3][3];
+  M2d_make_scaling(scaleMatrix, scaleFactor, scaleFactor);
+
+  // Rotate the polygon
+  double rotMat[3][3];
+  M2d_make_rotation_radians(rotMat, rotAmt);
+
+  //M2d_mat_mult_points(xs, ys, rotMat, xs, ys, pointCount); 
+
+  // Distance from center of poly to center of screen
+  double diffX = (midX * scaleFactor - sWidth / 2);
+  double diffY = (midY * scaleFactor - sHeight / 2);
+
+  // Move polygon to center of screen
+  /*
+  for (int i = 0; i < pointCount; i++) {
+    xs[i] -= diffX;
+    ys[i] -= diffY;
+  }
+  */
+  double centerMat[3][3];
+  M2d_make_translation(centerMat, diffX, diffY);
+
+  fscanf(file, "%d", &polygonCount[i]);
+
+
+  // Populate these arrays
+  for (int i = 0; i < polygonCount; i++) {
+    fscanf(file, "%d", &polygonPointCounts[i]);
+    for (int j = 0; j < polygonPointCounts[i]; j++) {
+      int pointIdx;
+      fscanf(file, "%d", &pointIdx);
+      polygonXs[i][j] = xs[pointIdx];
+      polygonYs[i][j] = ys[pointIdx];
+    }
+  }
+  
+  
+  double toCenter[3][3];
+  M2d_make_translation(toCenter, sWidth/2, sHeight/2);
+  double toOrigin[3][3];
+  M2d_make_translation(toOrigin, -midX, -midY);
+
+  //--------------------------------------------------------------------------
+  //Matrix Operations here!
+
+  M2d_print_mat(toCenter);
+  //M2d_print_mat(rotMat);
+  M2d_mat_mult(storageMat, toCenter, scaleMatrix);
+  //M2d_print_mat(storageMat);
+  M2d_mat_mult(storageMat, storageMat, toOrigin);
+  for(int i = 0; i < polygonCount; i++){
+    M2d_mat_mult_points(polygonXs[i], polygonYs[i], storageMat, polygonXs[i], polygonYs[i], polygonPointCounts[i]);
+
+  }
+
+
+  double centerToOrigin[3][3];
+  M2d_make_translation(centerToOrigin, -400, -400);
+  //Creating General Rotation Matrix
+  M2d_mat_mult(Rot, toCenter, rotMat);
+  M2d_mat_mult(Rot, Rot, centerToOrigin);
+
+  //M2d_print_mat(toCenter);
+ // M2d_print_mat(toOrigin);
+  M2d_print_mat(rotMat);
+  
+  //M2d_print_mat(scaleMatrix);
+  //M2d_print_mat(toOrigin);
+
+double r, g, b;
+  for (int i = 0; i < polygonCount; i++) {
+    fscanf(file, "%lf %lf %lf", &r, &g, &b);
+    colors[i][0] = r;
+    colors[i][1] = g;
+    colors[i][2] = b;
+  }
+
+  colorPoly();
+}
+  //--------------------------------------------------------------------------
+
+
+int main(int argc, char **argv) {
+  // Define screen width and height
+  double sWidth = 800;
+  double sHeight = 800;
+  G_init_graphics(sWidth, sHeight);
+
+  int numeralOneKeyCode = 49;
+  int keyCode = numeralOneKeyCode;
+  int prevKeyCode = -1;
+  double rotAmt = 0.1;
+
+  for(int i = 1; i < argc; i++)  {
+    collectPoints(argv[i], i);
+    
+
+  }
+    if (keyCode == prevKeyCode) {
+      printf("yesssssss\n");
+      
+      for(int i = 0; i < polygonCount; i++){
+	printf("here\n");
+	      M2d_mat_mult_points(polygonXs[i], polygonYs[i], Rot, polygonXs[i], polygonYs[i], polygonPointCounts[i]);
+} 
+       printf("plz\n");
+       colorPoly();
+       M2d_print_mat(Rot);
+
+    } else {
+      rotAmt = 0.1;
+    }
+	
+    prevKeyCode = keyCode;	
+    keyCode = G_wait_key();
+  
+}
